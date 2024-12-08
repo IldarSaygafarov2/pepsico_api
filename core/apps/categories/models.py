40032e269d7241e0bb8993b7c40e9435f589a0b0
IldarSaygafarov2 <sayildar17@gmail.com>
2024-12-08 14:04:47 +0500

    added category model

--- core/apps/categories/models.py
+++ core/apps/categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.apps.common.models import TimedBaseModel
+
+
+class Category(TimedBaseModel):
+    name = models.CharField(max_length=100, verbose_name='Название', unique=True)
+    banner_image = models.ImageField(upload_to='categories/banners/',
+                                     verbose_name='Фото заднего фона',
+                                     null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['-created_at']
